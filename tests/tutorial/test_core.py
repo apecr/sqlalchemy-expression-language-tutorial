af0bccb907001d9f31c3de7c9095fb72463c80af
apecr <alberto.eyo@gmail.com>
2020-06-10 16:46:39 +0200

    test(test_core.py) Closure to create tables
    Only calling create_tables once

--- tests/tutorial/test_core.py
+++ tests/tutorial/test_core.py
@@ -2,34 +2,39 @@ from tutorial.core import get_user_insert_string, create_tables, execute_insert,
     execute_insert_many_addresses
 
 
-class Table:
-    instance = None
+def create_tables_test():
+    tables = None
 
-    def __new__(cls, *args, **kwargs):
-        if not Table.instance:
+    def create_tab():
+        nonlocal tables
+        if tables is None:
             print('Creating tables')
-            Table.instance = create_tables()
-        return Table.instance
+            tables = create_tables()
+        return tables
+    return create_tab
+
+
+setup = create_tables_test()
 
 
 def test_create_tables():
-    metadata, users, addresses = Table()
+    metadata, users, addresses = setup()
     assert users is not None
 
 
 def test_get_user_insert_string():
-    metadata, users, addresses = Table()
+    metadata, users, addresses = setup()
     users_insert = get_user_insert_string(users)
     assert """INSERT INTO users (id, name, fullname) VALUES""" in users_insert
 
 
 def test_insert_element():
-    metadata, users, addresses = Table()
+    metadata, users, addresses = setup()
     result = execute_insert(engine=get_test_engine(), users_insert=get_user_insert(users))
     assert result is not None
 
 
 def test_insert_addresses():
-    metadata, users, addresses = Table()
+    metadata, users, addresses = setup()
     result = execute_insert_many_addresses(engine=get_test_engine(), address_table=addresses)
     assert result is not None
