16c56bcf3a8c8cc0157f1417ab87394f90546831
apecr <alberto.eyo@gmail.com>
2020-06-11 13:28:05 +0200

    chore() Include in Pypi

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=find_packages(exclude=("tests",)),
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     include_package_data=True,
     install_requires=["sqlalchemy"]
 )
